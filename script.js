1a13d4e0cf97b32190442a5128ddf3c1c9123f10
unknown <tanner@kopelfamily.com>
2022-04-29 11:18:20 -0500

    Add loading animation

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// LOADING ANIMATION
+readyToLoad = false;
+
+setTimeout(() => {
+    readyToLoad = true;
+}, 2000);
+
+$(document).ready(() => {
+    if (readyToLoad) {
+        $('#loading').css({'display': 'none'});
+    }
+
+    else {
+        setTimeout(() => {
+            $('#loading').css({'display': 'none'});
+        }, 2500);
+    }
+});
+
 // MOBILE MENU
 let menuVisible = false;
 
