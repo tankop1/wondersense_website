e59fb20eeddc3991e947db893af985c7d253dd57
unknown <tanner@kopelfamily.com>
2022-08-29 23:04:17 -0500

    Update native code

--- script.js
+++ script.js
@@ -27,4 +27,8 @@ $('#menu-icon').click(() => {
 });
 
 // POPUP
-$('#gallery-next').click(() => alert("The Heist VR is almost out, come back later for access to the game."));
+//$('#new-game').click(() => alert("[COMING SOON] isn't out yet, come back later for access to the game."));
+
+// LINK TO THE HEIST
+//$('#latest-game').click(() => window.open('https://www.oculus.com/experiences/quest/6808472739194566','_blank'));
+$('#heist-game').click(() => window.open('https://www.oculus.com/experiences/quest/6808472739194566','_blank'));
